dc1fcc19ef6022ea0ad53bef0162140732d5dbee
Nguyen Ba Luan <nguyenbaluan16@users.noreply.github.com>
2021-04-19 14:56:17 +0700

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,8 +1,30 @@
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.Scanner;
 
+import javax.swing.JOptionPane;
 public class main {
 
-	public main() {
-		// TODO Auto-generated constructor stub
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			FileInputStream fi = new FileInputStream("C:\\Users\\ABC\\eclipse-workspace\\Project\\src\\file.txt");
+			Scanner sc = new Scanner(fi);
+			while(sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		}catch(Exception e){
+			JOptionPane.showMessageDialog(null, "File khong ton tai!");
+		}
+		Graph g = new GraphMatrix(4);
+		g.add(0, 1);
+		g.add(0, 2);
+		g.add(1, 3);
+		g.add(2, 3);
+		g.printGraph();
+		
+		//Print the list of adjacent nodes of node 0
+		//System.out.println(g.adj(0));
 	}
 
 }
