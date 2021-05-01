e4ff39aeae8be7e9492aa9010ec37459cf9e1021
Nguyen Ba Luan <61571929+dongcuavodoi@users.noreply.github.com>
2021-05-01 12:07:40 +0700

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,12 +1,15 @@
+import java.io.FileInputStream;
 import java.util.*;
+
+import javax.swing.JOptionPane;
  
 //class to store edges of the weighted graph
 class Edge {
     int src, dest, weight;
-    Edge(int src, int dest, int weight) {
+    Edge(int src, int dest){   //, int weight) {
             this.src = src;
             this.dest = dest;
-            this.weight = weight;
+            //this.weight = weight;
         }
 }
 // Graph class
@@ -14,9 +17,9 @@ class Graph {
     // node of adjacency list 
     static class Node {
         int value, weight;
-        Node(int value, int weight)  {
+        Node(int value){ //, int weight)  {
             this.value = value;
-            this.weight = weight;
+           // this.weight = weight;
         }
     };
  
@@ -35,7 +38,7 @@ List<List<Node>> adj_list = new ArrayList<>();
         for (Edge e : edges)
         {
             // allocate new node in adjacency List from src to dest
-            adj_list.get(e.src).add(new Node(e.dest, e.weight));
+            adj_list.get(e.src).add(new Node(e.dest)); //, e.weight));
         }
     }
 // print adjacency list for the graph
@@ -47,8 +50,8 @@ List<List<Node>> adj_list = new ArrayList<>();
         while (src_vertex < list_size) {
             //traverse through the adjacency list and print the edges
             for (Node edge : graph.adj_list.get(src_vertex)) {
-                System.out.print("Vertex:" + src_vertex + " ==> " + edge.value + 
-                                " (" + edge.weight + ")\t");
+                System.out.print("Vertex:" + src_vertex + " ==> " + edge.value + "\t"); 
+                                //" (" + edge.weight + ")\t");
             }
  
             System.out.println();
@@ -56,17 +59,37 @@ List<List<Node>> adj_list = new ArrayList<>();
         }
     }
 }
-class Main{
+public class main{
     public static void main (String[] args) {
         // define edges of the graph 
-        List<Edge> edges = Arrays.asList(new Edge(0, 1, 2),new Edge(0, 2, 4),
-                   new Edge(1, 2, 4),new Edge(2, 0, 5), new Edge(2, 1, 4),
-                   new Edge(3, 2, 3), new Edge(4, 5, 1),new Edge(5, 4, 3));
- 
+    	char c,d;
+//        List<Edge> edges = Arrays.asList(new Edge(0, 1, 2),new Edge(0, 2, 4),
+//                   new Edge(1, 2, 4),new Edge(2, 0, 5), new Edge(2, 1, 4),
+//                   new Edge(3, 2, 3), new Edge(4, 5, 1),new Edge(5, 4, 3));
+    	List<Edge> edges = new ArrayList<Edge>();  //copy
+//    	edges.add(new Edge(1,2));
+//    	edges.add(new Edge(2,3));
+//    	edges.add(new Edge(2,4));
+//    	edges.add(new Edge(3,4));
+    	try {
+    		FileInputStream fi = new FileInputStream("C:\\Users\\ABC\\eclipse-workspace\\OOP_MidTerm\\src\\file.txt");
+    		Scanner sc = new Scanner(fi);
+    		while(sc.hasNextLine()) {
+    			String str = sc.nextLine();
+    			str = str.replaceAll("\\s+", "");
+    			for(int i = 0; i < str.length() - 1; i++) {
+    				c = str.charAt(i);
+    				d = str.charAt(i+1);
+    				edges.add(new Edge(Character.getNumericValue(c),Character.getNumericValue(d)));
+    			}
+    		}
+    		
+    	} catch (Exception e) {
+    		JOptionPane.showMessageDialog(null, "File khong ton tai");
+    	}
         // call graph class Constructor to construct a graph
         Graph graph = new Graph(edges);
  
         // print the graph as an adjacency list
-        Graph.printGraph(graph);
-    }
+        Graph.printGraph(graph);    }
 }
